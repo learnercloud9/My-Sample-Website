819fa744bccba23ffad4b2924cd45f1d939b7829
Mukul <mukul.s@taashee.com>
2025-09-19 10:54:49 +0530

    jh commit

--- scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Stopping Apache and cleaning old files..."
-sudo systemctl stop httpd
-rm -rf /var/www/html/*
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "Starting Apache..."
 sudo systemctl start httpd
 sudo systemctl enable httpd
